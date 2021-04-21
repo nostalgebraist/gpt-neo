99f13a847e3ce5e56133b686d7d80f870a090066
nostalgebraist <nostalgebraist@gmail.com>
2021-04-20 19:39:29 -0700

    sync

--- rob_util.py
+++ rob_util.py
@@ -16,3 +16,20 @@ def load_scalar_summaries(path):
                     loaded[e.step][v.tag] = array.item()
 
     return pd.DataFrame.from_dict(loaded, orient="index")
+
+
+def load_multi(prefix, fns, extras=[], alpha=0.99):
+  paths = [prefix + fn for fn in fns] + list(extras)
+  df = pd.concat([load_scalar_summaries(path) for path in paths]).sort_index()
+  df = df[~df.index.duplicated(keep="first")]
+
+  df.loc[df.S_noise > 100, 'G_noise'] = None
+  df.loc[df.S_noise > 100, 'S_noise'] = None
+
+  df['avg_G_noise'] = df.G_noise.ewm(alpha=1-alpha).mean()
+  df['avg_S_noise'] = df.S_noise.ewm(alpha=1-alpha).mean()
+
+  df["B_simple"] = df.avg_S_noise / df.avg_G_noise
+  df["B_inst"] = df.S_noise / df.G_noise
+
+  return df

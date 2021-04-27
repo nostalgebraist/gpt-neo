209d50d2a6194014524b474eb2ef4ec092367965
nostalgebraist <nostalgebraist@gmail.com>
2021-04-26 19:34:45 -0700

    work

--- inputs.py
+++ inputs.py
@@ -153,14 +153,15 @@ def pred_input(params, logger, enc=None,
 
     if params['file_dataset']:
         filenames = tf.io.gfile.glob(path_to_prompt)
-        dataset = tf.data.TFRecordDataset(filenames, buffer_size=0)
+        dataset = tf.data.Dataset.from_tensor_slices(filenames)
+        dataset = dataset.apply(partial(tf.data.TFRecordDataset, buffer_size=0))
+        # dataset = tf.data.TFRecordDataset(filenames, buffer_size=0)
         dataset = dataset.map(_parse_function, num_parallel_calls=1)
 
         dataset = dataset.batch(params['predict_batch_size'], drop_remainder=False)
         dataset = dataset.map(
             partial(_ensure_static_shape, batch_size=params['predict_batch_size'], n_ctx=params['n_ctx'])
         )
-        dataset = dataset.repeat()
     else:
         text = unicorns if path_to_prompt == "" else open(
             path_to_prompt, "r").read()
